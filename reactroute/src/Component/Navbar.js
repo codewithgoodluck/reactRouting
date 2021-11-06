c6532468502815e355fee54651cc27c141793f41
Goodluck <goodluckmordi4@gmail.com>
2021-11-06 08:03:36 +0100

    My first Commit

---
+++ reactroute/src/Component/Navbar.js
@@ -0,0 +1,15 @@
+import React, {useState}from 'react';
+import {Link} from 'react-router-dom'
+import Button from "./Button"
+import './Navbar.css'
+import Dropdown from "./Dropdown"
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
